714ab9532dc980a2b09b1bafeb815a7bb10b12c1
Mohammed shahid <www.mohdshahid303@gmail.com>
2025-01-22 00:27:11 +0530

    config.js changes for the next js deployment

--- public-portal/src/app/services/config.js
+++ public-portal/src/app/services/config.js
@@ -1,11 +1,12 @@
 const config_urls = {
   local: "http://localhost:3000",
   development: "http://localhost:8000",
-  production: process.env.NEXT_PUBLIC_SERVER_URL_API,
+  production:
+    process.env.NEXT_PUBLIC_SERVER_URL_API || "https://mosque.dev.mooo.com",
 };
 
 export const BASE_URL =
-  config_urls[process.env.NEXT_PUBLIC_DEVELOPMENT_MODE || "development"];
+  config_urls[process.env.NEXT_PUBLIC_DEVELOPMENT_MODE || "production"];
 export const API_URL =
-  config_urls[process.env.NEXT_PUBLIC_DEVELOPMENT_MODE || "development"] +
+  config_urls[process.env.NEXT_PUBLIC_DEVELOPMENT_MODE || "production"] +
   "/api/v1";
